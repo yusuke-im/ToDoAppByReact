c47ec7755a39f2ecfe7179c7048ad7b150bdd1a6
yusuke-im <imt.yusuke@gmail.com>
2024-08-15 09:23:06 +0900

    ToDo listの雛形をjsxで作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,5 +4,42 @@ import viteLogo from '/vite.svg'
 import './Todo.css'
 
 export const Todo = () => {
-  return <div>hoge</div>
-}
+  return (
+    <>
+    <div>
+      <input placeholder="Input Todo" />
+      <button>Add</button>
+    </div>
+
+    <div>
+      <p>Incomplete Todo</p>
+      <ul>
+        <li>
+          <p>Todo</p>
+          <button>Done</button>
+          <button>Delete</button>
+        </li>
+        <li>
+          <p>Todo</p>
+          <button>Done</button>
+          <button>Delete</button>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+    <p>complete Todo</p>
+      <ul>
+        <li>
+          <p>Todo</p>
+          <button>Restore</button>
+        </li>
+        <li>
+          <p>Todo</p>
+          <button>Restore</button>
+        </li>
+      </ul>
+    </div>
+    </>
+  );
+};
