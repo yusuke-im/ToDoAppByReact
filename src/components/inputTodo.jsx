23b3a871383b49c71e79e54974b2d7289535a30c
yusuke-im <imt.yusuke@gmail.com>
2024-08-15 14:39:05 +0900

    Component内でのcss定義

--- src/components/inputTodo.jsx
+++ src/components/inputTodo.jsx
@@ -1,14 +1,22 @@
+const style = {
+  backgroundColor: "#c6e5d9",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+}
+
 export const InputTodo = (props) => {
     const { todoText, onChange, onClick } = props;
     return (
-        <div　className='input-area'>
-
+        <div style={style}>
         <input 
             placeholder="Input Todo"
             value={todoText}
             onChange={onChange}
         />
-        
+
         <button onClick={onClick}>Add</button>
       </div>
     );
