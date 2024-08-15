eadbe715c09bc4eeef801d3a6b235f3c8b6812d6
yusuke-im <imt.yusuke@gmail.com>
2024-08-15 09:12:33 +0900

    雛形をToDo App向けに整形

---
+++ src/Todo.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './Todo.css'
+
+export const Todo = () => {
+  return <div>hoge</div>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Todo } from  './Todo.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Todo />
   </StrictMode>,
 )
